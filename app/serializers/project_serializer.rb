bbd52d2b2b923669270762213cadf5949ce61a11
sameera91 <sameera.bachoti@gmail.com>
2016-06-15 01:44:06 -0400

    added jquery to categories page

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -2,4 +2,5 @@ class ProjectSerializer < ActiveModel::Serializer
   attributes :id, :title, :short_blurb, :likes
   has_one :creator, :class_name => "User"
   has_many :comments
+  has_many :categories
 end
