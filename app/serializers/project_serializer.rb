44948d962651c08c50f626dd379ecee75b4cc13f
sameera91 <sameera.bachoti@gmail.com>
2016-06-16 15:28:08 -0400

    page updated with comment and edit/delete links when posting comment

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,5 +1,5 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :title, :short_blurb, :likes
+  attributes :id, :title, :short_blurb, :likes, :location
   has_one :creator, :class_name => "User"
   has_many :comments
   has_many :categories
