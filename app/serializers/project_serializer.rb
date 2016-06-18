f29ce0656894ed1e1355db6f2ff6d4aa99c1d501
sameera91 <sameera.bachoti@gmail.com>
2016-06-17 20:34:24 -0700

    added links for each project title on categories index page

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,5 +1,5 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :title, :short_blurb, :likes, :location
+  attributes :id, :title, :short_blurb, :likes, :location, :categories
   has_one :creator, :class_name => "User"
   has_many :comments
   has_many :categories
